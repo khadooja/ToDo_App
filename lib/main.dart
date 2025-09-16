ff11ba1aeedd85513cfe2e790f797930667dd5c6
Khadija <@DESKTOP-FVA8FDT>
2025-09-16 20:54:51 +0300

    GetX theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:todo/services/theme_services.dart';
+import 'package:todo/ui/theme.dart';
 
 import 'ui/pages/home_page.dart';
 
@@ -13,10 +15,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      theme: ThemeData(primaryColor: Colors.teal,
-        primarySwatch: Colors.teal,
-        
-      ),
+      theme: Themes.light,
+      darkTheme: Themes.dark,
+      themeMode: ThemeServices().theme,
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       home: const HomePage(),
--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:todo/services/theme_services.dart';
+import 'package:todo/ui/pages/notification_screen.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -11,7 +14,17 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back_ios, color: Colors.white),
+          onPressed: () {
+            ThemeServices().switchTheme();
+            Get.to(() => const NotificationScreen(
+                  payload: "Title|Body|3:00 PM",
+                ));
+          },
+        ),
+      ),
       body: Container(),
     );
   }
